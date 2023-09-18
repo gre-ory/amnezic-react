49c6ceab7446cb1eb089b90b734645ab273c8130
gre_ory <gregory.valigiani@gmail.com>
2023-09-18 19:02:38 +0200

    Updates

---
+++ precache-manifest.d611e3d00f12c28dd684b01978d74d4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9cd5f790c3c01217718",
+    "url": "/amnezic/static/css/main.ef352877.chunk.css"
+  },
+  {
+    "revision": "b9cd5f790c3c01217718",
+    "url": "/amnezic/static/js/main.edb93d43.chunk.js"
+  },
+  {
+    "revision": "4e11e89bc770d3a7a504",
+    "url": "/amnezic/static/js/runtime~main.1d8fcf87.js"
+  },
+  {
+    "revision": "6f56f18b31c399103607",
+    "url": "/amnezic/static/js/2.ffcba8c4.chunk.js"
+  },
+  {
+    "revision": "45a44973decd2da23517b488412ff356",
+    "url": "/amnezic/static/media/music.45a44973.png"
+  },
+  {
+    "revision": "e3d969ddaeb70007cd0215baa6c7536a",
+    "url": "/amnezic/static/media/avatars-dhigital.e3d969dd.webp"
+  },
+  {
+    "revision": "78c24c891d5c98db03c7ce0e5db81aa6",
+    "url": "/amnezic/index.html"
+  }
+];
