10996b177cd73fcba9b479237bce929d9b36938a
Gregory VALIGIANI <97035355+gvaligiani@users.noreply.github.com>
2023-09-25 12:30:07 +0200

    [deploy] (#3)
    Co-authored-by: gre_ory <gregory.valigiani@gmail.com>

---
+++ build-stg/precache-manifest.995ae28874f59ed8f53765746379e32c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "427a83e0d177b27590ce",
+    "url": "/amnezic/static/css/main.ef352877.chunk.css"
+  },
+  {
+    "revision": "427a83e0d177b27590ce",
+    "url": "/amnezic/static/js/main.3a63b3bc.chunk.js"
+  },
+  {
+    "revision": "4e11e89bc770d3a7a504",
+    "url": "/amnezic/static/js/runtime~main.1d8fcf87.js"
+  },
+  {
+    "revision": "944ce588fc22123405a5",
+    "url": "/amnezic/static/js/2.2f806996.chunk.js"
+  },
+  {
+    "revision": "45a44973decd2da23517b488412ff356",
+    "url": "/amnezic/static/media/music.45a44973.png"
+  },
+  {
+    "revision": "e3d969ddaeb70007cd0215baa6c7536a",
+    "url": "/amnezic/static/media/avatars-dhigital.e3d969dd.webp"
+  },
+  {
+    "revision": "ba3b4c701e930d662a17d177bba1a5ec",
+    "url": "/amnezic/index.html"
+  }
+];
