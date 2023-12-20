9f626de318b2d1619beb822f3cddddd4da747fde
gre_ory <gregory.valigiani@gmail.com>
2023-12-20 17:05:34 +0100

    [admin] playlist

---
+++ build-prd/precache-manifest.1025594ea8a16d5cfa0e9421095b1449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64db5f7ac44cab799551",
+    "url": "./static/css/main.79ba28bf.chunk.css"
+  },
+  {
+    "revision": "64db5f7ac44cab799551",
+    "url": "./static/js/main.45106608.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "aea227f1dc8efab63103",
+    "url": "./static/js/2.12e6d3d9.chunk.js"
+  },
+  {
+    "revision": "45a44973decd2da23517b488412ff356",
+    "url": "./static/media/music.45a44973.png"
+  },
+  {
+    "revision": "e3d969ddaeb70007cd0215baa6c7536a",
+    "url": "./static/media/avatars-dhigital.e3d969dd.webp"
+  },
+  {
+    "revision": "6f26c73f31f9d379ecf466c32e3ebfb6",
+    "url": "./index.html"
+  }
+];
